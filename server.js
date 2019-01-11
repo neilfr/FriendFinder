a236d157e72cc10c371cf781f73e4d552916ba6c
Neil Froggatt <neilfr@live.com>
2019-01-11 13:52:03 -0500

    initial file structure, hello world html, test route in a separate file

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+var PORT = 3000;
+
+var myStuff = require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function(){
+    console.log("App is listening on PORT " +PORT);
+});
+
