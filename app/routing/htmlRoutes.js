0abec88d7c752373c97130e8a31bea3c62277a7e
Neil Froggatt <neilfr@live.com>
2019-01-11 16:09:40 -0500

    added routes for css files

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,19 @@
+var path = require('path');
+var appPath = path.join(__dirname, '..');
 module.exports = function(app){
     app.get("/", function(req,res){
-        res.send("Welcome to Friend Finder!");
+        //res.send("Welcome to Friend Finder!");
+        console.log("Home!");
+        res.sendFile(path.join(appPath, '/public','home.html'));
     });
+    app.get("/css/reset.css",function(req,res){
+        console.log("Reset!");
+        res.sendFile(path.join(appPath,'/public/css','reset.css'));
+    });
+    app.get("/css/style.css",function(req,res){
+        console.log("CSS!");
+        res.sendFile(path.join(appPath,'/public/css','style.css'));
+    });
+    
 }
 
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+
+
 var PORT = 3000;
 
 var myStuff = require("./app/routing/htmlRoutes.js")(app);
