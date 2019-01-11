59d96845bfa92f3c9fa78d1a0d22550b998c08fa
Neil Froggatt <neilfr@live.com>
2019-01-11 16:24:13 -0500

    using filename parameter for multiple css files

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,14 +6,13 @@ module.exports = function(app){
         console.log("Home!");
         res.sendFile(path.join(appPath, '/public','home.html'));
     });
-    app.get("/css/reset.css",function(req,res){
-        console.log("Reset!");
-        res.sendFile(path.join(appPath,'/public/css','reset.css'));
-    });
-    app.get("/css/style.css",function(req,res){
+
+    app.get("/css/:filename",function(req,res){
         console.log("CSS!");
-        res.sendFile(path.join(appPath,'/public/css','style.css'));
+        var filename=req.params.filename;
+        console.log(filename);
+        res.sendFile(path.join(appPath,'/public/css',filename));
     });
-    
+ 
 }
 
