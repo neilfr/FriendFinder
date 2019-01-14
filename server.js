0538d6b1fca59f4010eb073a69c025e758cbad33
Neil Froggatt <neilfr@live.com>
2019-01-14 13:35:18 -0500

    added port change in server.js for heroku

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var app = express();
 
-var PORT = 3000;
+//var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var htmlRoutes = require("./app/routing/htmlRoutes.js");
 htmlRoutes(app);
